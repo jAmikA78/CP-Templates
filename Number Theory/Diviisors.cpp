81d7d06afac88268e33b613d47d7c00e72b6407e
jamika78 <ahmed.ibrahim7893@gmail.com>
2023-12-10 23:37:45 +0200

    jamika first commit

--- Number Theory/Diviisors.cpp
+++ Number Theory/Diviisors.cpp
@@ -15,17 +15,22 @@ using namespace std;
 #define kill(D) return void(cout << D << ln)
 #define alr(vv) ((vv).rbegin()), ((vv).rend())
 #define fix(pre) cout << setprecision(pre) << fixed;
-#define ai(v1, n) for (int ele = 0; ele < n; ele++) cout << v1[ele];
+#define ai(v1, n)                     \
+    for (int ele = 0; ele < n; ele++) \
+        cout << v1[ele];
 
 typedef long long ll;
 
 const ll mod = 1e9 + 7;
 const int maxSize = 1e6 + 10;
-
-vector<ll> divisors(ll n) {
+const int iii = 5;
+vector<ll> divisors(ll n)
+{
     vector<ll> ret;
-    for (ll i = 1; (i * i) <= n; i++) {
-        if (n % i == 0) {
+    for (ll i = 1; (i * i) <= n; i++)
+    {
+        if (n % i == 0)
+        {
             ret.pb(i);
             if (i * i != n)
                 ret.pb(n / i);
@@ -34,13 +39,14 @@ vector<ll> divisors(ll n) {
     return ret;
 }
 
-void s0lve() {
-
+void s0lve()
+{
 }
 
 void _3bbas_();
 
-signed main() {
+signed main()
+{
     _3bbas_();
     int tst = 1;
     cin >> tst;
@@ -49,10 +55,10 @@ signed main() {
     return 0;
 }
 
-void _3bbas_() {
+void _3bbas_()
+{
     cin.tie(0)->sync_with_stdio(0);
 #ifndef ONLINE_JUDGE
     freopen(".out", "w", stdout);
 #endif
 }
-
